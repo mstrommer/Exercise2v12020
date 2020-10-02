161b58ef51770456ba59f85e97887ef0ecc4647b
Leon Freudenthaler <leon.freudenthaler@stud.fh-campuswien.ac.at>
2020-10-02 09:38:37 +0200

    tested first 3 tasks and added test cases

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -8,16 +8,73 @@ public class App {
     //todo Task 1
     public void largestNumber(){
         // input your solution here
+        double input;
+        double max = 0;
+        int count = 1;
+
+        Scanner scan = new Scanner(System.in);
+        scan.useLocale(Locale.US);  //enable us float input; ex: 2.2
+
+        do {
+            //Todo remove println for successful tests
+            System.out.print("Number " + count + ": ");
+
+            if (scan.hasNextDouble()) {         //check if is valid double
+                input = scan.nextDouble();
+
+                if(input > max){
+                    max = input;
+                }
+                count++;
+            } else {
+                input = 0;
+            }
+
+        } while (input > 0);
+
+        if(max <= 0){
+            System.out.println("No number entered.");
+        } else {
+            System.out.println("The largest number is " + String.format(Locale.US,"%.2f", max));
+        }
     }
 
     //todo Task 2
     public void stairs(){
         // input your solution here
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("n: ");
+
+        int input = scan.nextInt();
+        int count = 1;
+
+        if(input > 0){
+            for (int i = 0; i < input; i++){
+                for (int j = 0; j <= i; j++){
+                    System.out.print(count + " ");
+                    count++;
+                }
+                System.out.println();
+            }
+        } else {
+            System.out.println("Invalid number!");
+        }
     }
 
     //todo Task 3
     public void printPyramid(){
         // input your solution here
+        final int rows = 6;
+        for (int row = 1; row <= rows; row++){
+            for (int col = rows; col > row; col--) {
+                System.out.print(" ");
+            }
+            for (int col = 0; col < row*2-1; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
     //todo Task 4
--- src/test/java/at/ac/fhcampuswien/AppTest.java
+++ src/test/java/at/ac/fhcampuswien/AppTest.java
@@ -81,6 +81,58 @@ class AppTest {
     }
 
     @Test
+    public void largestNumber3()
+    {
+        ps.println("some string");
+
+        String output = "Number 1: " +
+                "No number entered." + System.lineSeparator();
+
+        // action
+        App exercise1 = new App();
+        exercise1.largestNumber();
+
+        // assertion
+        assertEquals(output, bos.toString());
+    }
+
+    @Test
+    public void largestNumber4()
+    {
+        ps.println(2);
+        ps.println("some string");
+
+        String output = "Number 1: " +
+                "Number 2: " +
+                "The largest number is 2.00" + System.lineSeparator();
+
+        // action
+        App exercise1 = new App();
+        exercise1.largestNumber();
+
+        // assertion
+        assertEquals(output, bos.toString());
+    }
+
+    @Test
+    public void largestNumber5()
+    {
+        ps.println(3.3);
+        ps.println(-3);
+
+        String output = "Number 1: " +
+                "Number 2: " +
+                "The largest number is 3.30" + System.lineSeparator();
+
+        // action
+        App exercise1 = new App();
+        exercise1.largestNumber();
+
+        // assertion
+        assertEquals(output, bos.toString());
+    }
+
+    @Test
     public void stairs1()
     {
         ps.println("5");
